b7ce5495cdd4e2e45e5a5516aa445e86d8d3a8b2
Ngo Gia Huan <ngohuan122003@gmail.com>
2023-10-04 17:26:12 +0700

    fix navbar

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,58 +1,112 @@
-import Navbar from "react-bootstrap/Navbar";
-import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./Navbar.css";
-import { useContext } from "react";
+import React, { useContext } from "react";
+import {
+  AppBar,
+  Toolbar,
+  InputBase,
+  alpha,
+  styled,
+  Switch,
+  Grid,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 import { ThemeContext } from "../ThemeContext";
+import "./Navbar.css";
 import { Link } from "react-router-dom";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
 
 function NavbarComponent() {
   const { theme, toggle } = useContext(ThemeContext);
+
   return (
-    <Navbar
+    <AppBar
+      position="static"
       className="navbar"
       style={{ backgroundColor: theme.backgroundColor, color: theme.color }}
     >
-      <Col xs="auto">
+      <Toolbar className="toolbar">
         <ul className="nav-ul">
           <li>
-            <Link to='/' style={{ color: theme.color }}>Home</Link>
+            <Link to="/" style={{ color: theme.color }}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/news" style={{ color: theme.color }}>News</Link>
+            <Link to="/news" style={{ color: theme.color }}>
+              News
+            </Link>
           </li>
           <li>
-            <Link to="/about" style={{ color: theme.color }}>About</Link>
+            <Link to="/about" style={{ color: theme.color }}>
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/contact" style={{ color: theme.color }}>Contact</Link>
+            <Link to="/contact" style={{ color: theme.color }}>
+              Contact
+            </Link>
           </li>
-        </ul></Col>
-      <Col xs="auto" className="search">
-        <Row>
-          <Col xs="auto">
-            <Form inline>
-              <div class="textInputWrapper">
-                <input
-                  placeholder="Type Here"
-                  type="text"
-                  class="textInput"
-                ></input>
-              </div>
-            </Form>
-          </Col>
+        </ul>
 
-          <Col xs="auto button-container">
-            <label class="switch">
-              <input type="checkbox" onClick={toggle}></input>
-              <span class="slider"></span>
-            </label>
-          </Col>
-        </Row>
-      </Col>
-    </Navbar>
+        <Grid container alignItems="center" style={{ width: "auto" }}>
+          <Grid item>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+          </Grid>
+          <Grid item>
+            <div className="button-container">
+              <Switch color="default" onChange={toggle} />
+            </div>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
 }
 
